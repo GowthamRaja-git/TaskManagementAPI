34233acd1d1a27ad1a972735e93ac3316f3f25c8
Gowtham Raja <99356838+GowthamRaja-git@users.noreply.github.com>
2025-04-26 03:10:20 +0530

    Updated Models

---
+++ Models/TaskItem.cs
@@ -0,0 +1,54 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TaskManagementAPI.Models
+{
+    public class TaskItem
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Title { get; set; }
+
+        [StringLength(500)]
+        public string Description { get; set; }
+
+        [Required]
+        public int AssignedToUserId { get; set; }
+        //public User AssignedTo { get; set; }
+        public User? AssignedToUser { get; set; }
+    }
+
+
+
+    public class TaskItemRequest
+    {
+
+        [Required]
+        [StringLength(100)]
+        public string Title { get; set; }
+
+        [StringLength(500)]
+        public string Description { get; set; }
+
+        [Required]
+        public int AssignedToUserId { get; set; }
+    }
+
+    public class UserDetails
+    {
+        public int UserID { get; set; }
+        public string Username { get; set; }
+        //public string Password { get; set; }
+        public Role RoleID { get; set; }
+    }
+
+    public class TaskResponse
+    {
+        public int TaskID { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public int AssignedToUserId { get; set; }
+        public UserDetails? UserDetails { get; set; }
+    }
+}
---
+++ Models/User.cs
@@ -0,0 +1,21 @@
+﻿using System.Data;
+
+namespace TaskManagementAPI.Models
+{
+
+    public enum Role { Admin, User }
+    public class User
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        //public string Password { get; set; }
+        public Role Role { get; set; }
+    }
+
+    public class UserTask
+    {
+        public int TaskID { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+    }
+}
